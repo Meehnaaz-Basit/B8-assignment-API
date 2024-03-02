fac83d5638bde17a9150dbaac3dfbc170a01f9c4
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-03-02 14:58:03 +0600

    added verified mark

--- js/script.js
+++ js/script.js
@@ -7,28 +7,48 @@ const fetchCategory = async () => {
   const allData = data.data;
   //   console.log(allData);
   const btnContainer = document.getElementById("catBtn");
+
   allData.forEach((category) => {
+    // console.log(category);
     const catBtn = document.createElement("button");
     catBtn.classList.add("btn");
     catBtn.innerText = category.category;
+    catBtn.addEventListener("click", () => fetchMusic(category.category_id));
     btnContainer.appendChild(catBtn);
   });
 };
 
 // fetch youtube music
-const fetchMusic = async () => {
+const fetchMusic = async (catID) => {
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/videos/category/1000"
+    `https://openapi.programming-hero.com/api/videos/category/${catID}`
   );
   const data = await response.json();
   const musicData = data.data;
   //   console.log(musicData);
   const vdoSection = document.getElementById("vdoSection");
-  musicData.forEach((musicVdo) => {
-    console.log(musicVdo);
-    const musicCat = document.createElement("div");
-    musicCat.classList = "card bg-base-100 shadow-xl";
-    musicCat.innerHTML = `
+  vdoSection.innerHTML = "";
+
+  if (musicData.length === 0) {
+    // If there are no elements for this category, display placeholder
+    const placeholder = document.createElement("div");
+    placeholder.innerHTML = `
+      <div class="card">
+        <figure>
+          <img class="h-40 overflow-hidden" src="Group 18.png" alt="Placeholder" />
+        </figure>
+        <div class="card-body p-6">
+          <p class="text-lg font-bold">Sorry, there is no content.</p>
+        </div>
+      </div>
+    `;
+    vdoSection.appendChild(placeholder);
+  } else {
+    musicData.forEach((musicVdo) => {
+      console.log(musicVdo);
+      const musicCat = document.createElement("div");
+      musicCat.classList = "card bg-base-100 shadow-xl";
+      musicCat.innerHTML = `
             <figure>
               <img class="h-40 overflow-hidden"
                 src="${musicVdo.thumbnail}"
@@ -38,24 +58,34 @@ const fetchMusic = async () => {
             <div class="card-body p-6">
               <div class="flex gap-4">
                 <div class="basis-1/4">
-                  <img class=" w-12 h-12 rounded-full" src="${musicVdo.authors[0].profile_picture}" alt="" />
+                  <img class=" w-12 h-12 rounded-full" src="${
+                    musicVdo.authors[0].profile_picture
+                  }" alt="" />
                 </div>
                 <div class="space-y-2">
                   <h2 class="card-title text-lg">
                     ${musicVdo.title}
                   </h2>
                   <div class="flex gap-4">
-                    <span class="author-name">${musicVdo.authors[0].profile_name}</span>
-                    <span><img src="Group.jpeg" alt="" /></span>
+                    <span class="author-name">${
+                      musicVdo.authors[0].profile_name
+                    }</span>
+                    
+                    ${
+                      musicVdo.authors[0].verified
+                        ? `<span><img src="Group.jpeg" alt="" /></span>`
+                        : ""
+                    }
                   </div>
                   <p>${musicVdo.others.views} views</p>
                 </div>
               </div>
             </div>
     `;
-    vdoSection.appendChild(musicCat);
-  });
+      vdoSection.appendChild(musicCat);
+    });
+  }
 };
 
 fetchCategory();
-fetchMusic();
+fetchMusic(1000);
